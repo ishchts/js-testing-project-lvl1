ccfc2d5819de8f4a20b310fa8c263403fcc9860c
Ilya Shchutskiy <i1ya1l@yandex.ru>
2022-05-18 13:28:40 +0300

    feat(step-6): develop

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
+import { program } from 'commander';
+import { cwd } from 'process';
+// eslint-disable-next-line import/extensions
+import load from '../src/loader.js';
 
-console.log(1111);
+const run = async (url, options) => {
+  try {
+    const { output } = options;
+    console.log(await load(url, output));
+  } catch (e) {
+    console.error(e.message);
+    process.exit(1);
+  }
+};
+
+program
+  .description('Site loader')
+  .version('1.0.0')
+  .arguments('<url>')
+  .option('-o, --output <v>', 'output path', cwd())
+  .action(run)
+  .parse();

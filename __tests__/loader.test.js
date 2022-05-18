b78f4b9a6019bc62b13ac64dd323d28d1d35023b
Ilya Shchutskiy <i1ya1l@yandex.ru>
2022-05-18 10:48:37 +0300

    feat(step-2): develop

--- __tests__/loader.test.js
+++ __tests__/loader.test.js
@@ -6,26 +6,51 @@ import { readFile, mkdtemp } from 'fs/promises';
 import pageLoader from '../src/loader';
 
 const readFixture = async (...paths) => readFile(join(__dirname, '..', '/__fixtures__', ...paths), 'utf-8');
+/*
+nock.disableNetConnect();
+(async () => {
+  const resources = ['img.png'];
+  const siteHtml = await readFixture('site.html');
+  nock('https://ru.hexlet.io').get('/courses').reply(200, siteHtml);
+  await Promise.all(resources.map(async (resource) => {
+    const content = await readFixture(resource);
+    nock('https://ru.hexlet.io').get(`/${resource}`).reply(200, content);
+  }));
+
+  await pageLoader('https://ru.hexlet.io/courses', '/var/tmp');
+})();
+*/
+
+beforeEach(() => {
+  nock.disableNetConnect();
+});
+
+afterEach(() => {
+  nock.cleanAll();
+  nock.enableNetConnect();
+});
 
 describe('positive cases', () => {
+  const resources = ['nodejs.png'];
   let path;
-  beforeEach(async () => {
-    nock.disableNetConnect();
-    path = await mkdtemp(join(tmpdir(), 'page-loader-'));
-  });
 
-  afterEach(() => {
-    nock.cleanAll();
-    nock.enableNetConnect();
+  beforeEach(async () => {
+    path = await mkdtemp(join(tmpdir(), 'page-loader-'));
+    const siteHtml = await readFixture('site.html');
+
+    nock('https://ru.hexlet.io').get('/courses').reply(200, siteHtml);
+    await Promise.all(resources.map(async (resource) => {
+      const content = await readFixture(resource);
+      nock('https://ru.hexlet.io').get(`/${resource}`).reply(200, content);
+    }));
   });
 
   test('loader', async () => {
-    const siteHtml = await readFixture('site.html');
-    nock('https://ru.hexlet.io').get('/courses').reply(200, siteHtml);
+    const loadedHtml = await readFixture('expected', 'site.html');
+    const result = await pageLoader('https://ru.hexlet.io/courses', path);
+    const readResult = await readFile(result, 'utf-8');
 
-    const file = await readFile(await pageLoader('https://ru.hexlet.io/courses', path), 'utf-8');
-    console.log('siteHtml', siteHtml);
-    console.log('path', path);
-    expect(file).toEqual(siteHtml);
+    expect(result).toEqual(join(path, 'ru-hexlet-io-courses.html'));
+    expect(readResult).toEqual(loadedHtml);
   });
 });
--- src/loader.js
+++ src/loader.js
@@ -34,11 +34,44 @@ const saveToFile = async (filePath, content) => {
   }
 };
 
+const toResource = (url) => ({
+  originUrl: url,
+  url: new URL(url, new URL(baseUrl).origin),
+  filePath: undefined,
+});
+
+const resourceIsLocal = (resource) => new URL(baseUrl).host === resource.url.host;
+
+const loadResource = async (resource) => {
+  const { data: content } = await axios.get(resource.url.href, { responseType: 'arraybuffer' });
+  const filePath = join(toFileName(baseUrl).replace('.html', '_files'), toFileName(resource.url.href));
+  await saveToFile(join(basePath, filePath), content);
+  return { ...resource, filePath };
+};
+
+const loadResources = async (getResourceUrls, replaceContent) => {
+  const resources = getResourceUrls()
+    .map((url) => toResource(url))
+    .filter((resource) => resourceIsLocal(resource));
+
+  const loadedResources = await Promise.all(resources.map(
+    async (resource) => loadResource(resource),
+  ));
+  loadedResources.forEach((resource) => replaceContent(resource));
+};
+
 export default async (url, path = '') => {
   baseUrl = url;
   basePath = path;
   const { data: content } = await axios.get(baseUrl);
   const $ = cheerio.load(content);
+
+  const imgs = loadResources(
+    () => $('img').map((i, el) => $(el).attr('src')).toArray(),
+    (resource) => $(`img[src="${resource.originUrl}"]`).attr('src', resource.filePath),
+  );
+
+  await Promise.all([imgs]);
 
   const filePath = join(basePath, toFileName(baseUrl));
   await saveToFile(filePath, $.html());

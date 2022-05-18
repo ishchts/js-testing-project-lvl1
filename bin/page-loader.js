650a64185ad7ffb792452ee5965209d18d6afc31
Ilya Shchutskiy <i1ya1l@yandex.ru>
2022-05-18 13:52:28 +0300

    test

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,23 +1,16 @@
 #!/usr/bin/env node
-import { program } from 'commander';
-import { cwd } from 'process';
-// eslint-disable-next-line import/extensions
-import load from '../src/loader.js';
 
-const run = async (url, options) => {
-  try {
-    const { output } = options;
-    console.log(await load(url, output));
-  } catch (e) {
-    console.error(e.message);
-    process.exit(1);
-  }
-};
+import program from 'commander';
+
+import loader from '../src/loader';
 
 program
-  .description('Site loader')
-  .version('1.0.0')
-  .arguments('<url>')
-  .option('-o, --output <v>', 'output path', cwd())
-  .action(run)
-  .parse();
+  .description('Loads page')
+  .arguments('<pageUrl>')
+  .option('-o, --output [path]', 'choose output path', process.cwd())
+  .action(async (pageUrl, options) => {
+    console.log(await loader(pageUrl, options.output));
+  })
+  .parse(process.argv);
+
+if (!program.args.length) program.help();

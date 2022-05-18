120cf7bf0a9cfc13bf2cddca630feae65ff8f59d
Ilya Shchutskiy <i1ya1l@yandex.ru>
2022-05-18 13:14:14 +0300

    feat(step-4,5): develop

--- __tests__/loader.test.js
+++ __tests__/loader.test.js
@@ -6,20 +6,6 @@ import { readFile, mkdtemp } from 'fs/promises';
 import pageLoader from '../src/loader';
 
 const readFixture = async (...paths) => readFile(join(__dirname, '..', '/__fixtures__', ...paths), 'utf-8');
-/*
-nock.disableNetConnect();
-(async () => {
-  const resources = ['img.png'];
-  const siteHtml = await readFixture('site.html');
-  nock('https://ru.hexlet.io').get('/courses').reply(200, siteHtml);
-  await Promise.all(resources.map(async (resource) => {
-    const content = await readFixture(resource);
-    nock('https://ru.hexlet.io').get(`/${resource}`).reply(200, content);
-  }));
-
-  await pageLoader('https://ru.hexlet.io/courses', '/var/tmp');
-})();
-*/
 
 beforeEach(() => {
   nock.disableNetConnect();
@@ -52,5 +38,18 @@ describe('positive cases', () => {
 
     expect(result).toEqual(join(path, 'ru-hexlet-io-courses.html'));
     expect(readResult).toEqual(loadedHtml);
+  });
+});
+
+describe('negative cases', () => {
+  test.each([404, 500])('server %s error', (code) => {
+    nock('https://site.com').get('/path').reply(code);
+    return expect(pageLoader('https://site.com/path')).rejects.toThrow();
+  });
+
+  test('access error', async () => {
+    const html = await readFixture('simple-site.html');
+    nock('https://site.com').get('/path').reply(200, html);
+    return expect(pageLoader('https://site.com/path', '/sys')).rejects.toThrow();
   });
 });
